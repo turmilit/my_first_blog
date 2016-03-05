9fa3dac76f4de3c76c25006d0e9b865487f7cddb
turmilit <test@MacBook-Pro-Alexey.local>
2016-03-05 18:06:55 +0400

    added post publish date

--- blog/models.py
+++ blog/models.py
@@ -10,8 +10,10 @@ class Post(models.Model):
 	published_date = models.DateTimeField(
 		blank=True, null=True)
 
-	def publish(self):
-		self.published_date = timezone.now()
+	def publish(self, pub_date=None):
+		if pub_date == None:
+			pub_date = timezone.now()
+		self.published_date = pub_date
 		self.save()
 
 	def __str__(self):
